54e62063695b711748732c9b7f5047b83129a9f0
Zeus6768 <akaps6768@gmail.com>
2021-09-11 01:32:35 +0900

    First Commit

---
+++ photo/admin.py
@@ -0,0 +1,7 @@
+from photo.models import Photo
+from django.contrib import admin
+from .models import *
+
+# Register your models here.
+
+admin.site.register(Photo)
---
+++ photo/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+from .views import *
+
+app_name = 'photo'
+
+urlpatterns = [
+    path("create/", PhotoCreate.as_view(), name="create"),
+    path("delete/<int:pk>/", PhotoDelete.as_view(), name="delete"),
+    path("update/<int:pk>/", PhotoUpdate.as_view(), name="update"),
+    path("detail/<int:pk>/", PhotoDetail.as_view(), name="detail"),
+    path("", PhotoList.as_view(), name="index"),
+]
