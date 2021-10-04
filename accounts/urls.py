a6b3daeaff10adce44b27166f8c56c5a50ec1213
Zeus6768 <akaps6768@gmail.com>
2021-10-04 15:59:40 +0900

    1004 login.html, accounts/urls.py update

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,5 @@ app_name = "accounts"
 
 urlpatterns = [
 	path('login/', LoginView.as_view(template_name='accounts/login.html'), name='login'),
-	path('logout/', LogoutView.as_view(template_name='accounts/logout.html'), name='logout'),
+	path('logout/', LogoutView.as_view(template_name='accounts/login.html'), name='logout'),
 ]
